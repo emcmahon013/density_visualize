fa69517b6ccf370ca45299b94662ed7190feb7cc
Dingzeyu Li <lidingzeyu@gmail.com>
2015-04-30 13:13:49 -0400

    more updates on the date looping

--- density_includes.py
+++ density_includes.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python
-
 import json, requests
+import datetime
 
 def get_building_info():
   url = 'http://density.adicu.com/docs/building_info'
@@ -14,3 +13,25 @@ def get_building_info():
     #print  data["data"][i]["group_id"], ": ", data["data"][i]["group_name"]
   print "building info retrived"
   return building_info
+
+def get_json_url(start_date, start_hour, end_data, end_hour, group_id):
+  base_url = "http://density.adicu.com/window/%sT%s/%sT%s/group/%d?auth_token=9O4CRZLWI0OFZII4S4HYZNC60OZIZ3CB"
+
+  return base_url % (start_date, start_hour, end_data, end_hour, group_id)
+   
+def get_day_of_week(date):
+  tks = [int(i) for i in date.split("-")]
+  ans = datetime.date(tks[0], tks[1], tks[2])
+
+  return ans.strftime("%w") #%A
+
+def daterange(start_date, end_date):
+  tks = [int(i) for i in start_date.split("-")]
+  start_date = datetime.date(tks[0], tks[1], tks[2])
+
+  tks = [int(i) for i in end_date.split("-")]
+  end_date = datetime.date(tks[0], tks[1], tks[2])
+
+  for n in range(int ((end_date - start_date).days)):
+    yield start_date + datetime.timedelta(n)
+
--- gen_url.py
+++ gen_url.py
@@ -1,15 +1,28 @@
+#!/usr/bin/python
+
 from density_includes import *
 import json, requests
-from pprint import pprint
+import numpy as np
 
-build_info = get_building_info()
+#build_info = get_building_info()
 
-for 
+#http://density.adicu.com/window/2014-10-10T08:00/2014-10-10T21:30/group/147?auth_token=9O4CRZLWI0OFZII4S4HYZNC60OZIZ3CB
 
-url = 'http://density.adicu.com/latest?auth_token=9O4CRZLWI0OFZII4S4HYZNC60OZIZ3CB'
+get_day_of_week("2015-04-30")
 
-resp = requests.get(url=url)
-data = json.loads(resp.text)
+density = np.zeros((7, 24))
+density_count = np.zeros((7, 24))
 
-for i in xrange(0, len(data["data"])):
-  print  data["data"][i]["parent_name"]
+for single_date in daterange("2014-10-10", "2014-10-12"):
+	url = get_json_url(single_date, "00:00", single_date, "23:59", 130)
+	print url
+
+
+#print density
+#url = 'http://density.adicu.com/latest?auth_token=9O4CRZLWI0OFZII4S4HYZNC60OZIZ3CB'
+
+#resp = requests.get(url=url)
+#data = json.loads(resp.text)
+
+#for i in xrange(0, len(data["data"])):
+#  print  data["data"][i]["parent_name"]
